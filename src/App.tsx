9b5139798e907502da4d5552ae42b6f0515f7bcb
JoaoReis-code <jzreiis01@gmail.com>
2023-06-12 21:11:46 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,15 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
-import Simples from "./componentes/Simples";
-import Frag from "./componentes/Frag";
-import ParImpar from "./componentes/ParImpar";
-import Familia from "./componentes/relacao/Familia";
-import Membro from "./componentes/relacao/Membro";
-import UsuarioLogado from "./componentes/UsuarioLogado";
-import Botao from "./componentes/Botao";
-import Contador from "./componentes/Contador";
-import DigiteSeuNome from "./componentes/DigiteSeuNome";
-import DimensoesFixas from "./componentes/DimensoesFixas";
-import Mega from "./componentes/MegaSena/Mega";
-import Calculadora from "./componentes/Calculadora/Calculadora";
+import { NavigationContainer } from "@react-navigation/native";
+import { MainStackNavigator} from "./componentes/Menu/StackNavigator";
 
 export default class App extends Component {
   render() {
     return (
 
-      <View style={estilos.container}>
-
-        {/*<Simples texto='Rodrigo' /> 
-         <Frag titulo="Cadastro" 
-              subTitulo="Teda de Cadastro de Produto" /> 
-         <ParImpar numero={9}  /> 8 
-        <Familia>
-            <Membro nome="Ana" sobreNome="Silva"/>
-            <Membro nome="José" sobreNome="Silva"/> 
-        </Familia> 
-        <UsuarioLogado usuario={ {nome:'teste', email:'teste@teste.com'} } /> 
-        <UsuarioLogado usuario={ {nome:'teste 2'} } />
-        <UsuarioLogado usuario={ {email:'teste@teste.com'} } />        
-        <UsuarioLogado usuario={ {} } />
-        <UsuarioLogado usuario={ null } /> 
-        <Contador valorInicial={55} />         
-        <DigiteSeuNome /> 
-        <DimensoesFixas /> 
-        <Mega qtdeNumeros={7} />  
-        <Calculadora />
-        */}
-
-        
-
-      </View>
+        <NavigationContainer>
+          <MainStackNavigator/>
+        </NavigationContainer>
 
     );
   }
@@ -58,3 +25,4 @@ const estilos = StyleSheet.create({
     fontSize: 50,
   },
 });
+
